b2331608d5fe7db6fdd5ba7c8fb0506b999ed311
Alberto Sierra <alberto.sierra@bodybuilding.com>
2016-12-15 19:26:02 +0100

    remove tabs

--- 05_constants/constants.go
+++ 05_constants/constants.go
@@ -13,9 +13,9 @@ The initial value for any iota is 0
 */
 
 const (
-	a = iota // 0
-	b = iota // 1
-	c = iota // 2
+  a = iota // 0
+  b = iota // 1
+  c = iota // 2
 )
 
 /*
@@ -24,19 +24,19 @@ works as a blackhole for discarding return from a function
 */
 
 const (
-	_ = iota // discards value
-	d        // 1
-	e        // 2
+  _ = iota // discards value
+  d        // 1
+  e        // 2
 )
 
 func main() {
-	fmt.Println("First constant declaration:")
-	fmt.Printf("%s, %t\n", c1, c2)
-	fmt.Println("First iota declaration:")
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println("Second iota declaration:")
-	fmt.Println(d)
-	fmt.Println(e)
+  fmt.Println("First constant declaration:")
+  fmt.Printf("%s, %t\n", c1, c2)
+  fmt.Println("First iota declaration:")
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+  fmt.Println("Second iota declaration:")
+  fmt.Println(d)
+  fmt.Println(e)
 }
