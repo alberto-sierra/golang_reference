641e98af20495b4cb54656deca262ecfef99167f
Alberto Sierra <alberto.sierra@bodybuilding.com>
2016-12-15 19:26:44 +0100

    remove tabs

--- 03_scope/scope.go
+++ 03_scope/scope.go
@@ -9,11 +9,11 @@ in a variable.
 */
 
 func Wrapper() func() int {
-	x := 0
-	return func() int {
-		x++
-		return x
-	}
+  x := 0
+  return func() int {
+    x++
+    return x
+  }
 }
 
 /*
@@ -23,22 +23,22 @@ even with different signature (name + params + return)
 */
 
 func Wrapper2(x int) func() int {
-	return func() int {
-		x++
-		return x
-	}
+  return func() int {
+    x++
+    return x
+  }
 }
 
 func main() {
 
-	// the variable increment "becomes" a function.
-	var increment = Wrapper()
-	fmt.Println(increment())
-	fmt.Println(increment())
+  // the variable increment "becomes" a function.
+  var increment = Wrapper()
+  fmt.Println(increment())
+  fmt.Println(increment())
 
-	x := 0
-	increment = Wrapper2(x) // passing x as a value
-	fmt.Println(increment())
-	fmt.Println(increment())
+  x := 0
+  increment = Wrapper2(x) // passing x as a value
+  fmt.Println(increment())
+  fmt.Println(increment())
 
 }
