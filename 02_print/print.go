7c67653a7f4165d33cc38f5c2a893cea5bc2d34b
Alberto Sierra <alberto.sierra@bodybuilding.com>
2016-12-15 19:27:20 +0100

    remove tabs

--- 02_print/print.go
+++ 02_print/print.go
@@ -5,8 +5,8 @@ import "fmt"
 func print(text int) { fmt.Println(text) }
 
 func main() {
-	number := 1000
-	text := "Formatting and printing with Go."
-	fmt.Println(text)
-	fmt.Printf("%d - %b", number, number)
+  number := 1000
+  text := "Formatting and printing with Go."
+  fmt.Println(text)
+  fmt.Printf("%d - %b", number, number)
 }
