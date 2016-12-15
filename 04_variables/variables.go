604996182cde75943beb254208651d41da85b90e
Alberto Sierra <alberto.sierra@bodybuilding.com>
2016-12-15 19:26:20 +0100

    remove tabs

--- 04_variables/variables.go
+++ 04_variables/variables.go
@@ -9,19 +9,19 @@ var c string
 var d bool
 
 func stdeclaration() {
-	fmt.Printf("a = %d\nb = %f\nc = %s\nd = %t", a, b, c, d)
+  fmt.Printf("a = %d\nb = %f\nc = %s\nd = %t", a, b, c, d)
 }
 
 // Shorthand declaration (with assignment)
 func shorthand() { // only inside functions
-	e := 1
-	f := 1.0
-	g := "qwerty"
-	h := true
-	fmt.Printf("e = %d\nf = %f\ng = %s\nh = %t\n", e, f, g, h)
+  e := 1
+  f := 1.0
+  g := "qwerty"
+  h := true
+  fmt.Printf("e = %d\nf = %f\ng = %s\nh = %t\n", e, f, g, h)
 }
 
 func main() {
-	stdeclaration()
-	shorthand()
+  stdeclaration()
+  shorthand()
 }
